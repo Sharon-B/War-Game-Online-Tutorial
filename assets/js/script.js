daa598e42a3bd458b69d46811027ed8720c8805f
Sharon-B <sharonbagnall@gmail.com>
2020-12-16 19:09:57 +0000

    Add script to dynamically render the cards & remove card div from index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,13 @@
 
 import Deck from "./deck.js"
 
+
+const computerCardSlot = document.querySelector(".computer-card-slot")
+const playerCardSlot = document.querySelector(".player-card-slot")
+
 const deck = new Deck()
 deck.shuffle()
 console.log(deck.cards)
+
+computerCardSlot.appendChild(deck.cards[0].getHTML())
+playerCardSlot.appendChild(deck.cards[1].getHTML())
